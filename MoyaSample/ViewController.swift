e1436e3dcd51c9a983f49102e6ffdde2e2371103
touyou <fujiyou1121@gmail.com>
2019-07-17 13:37:37 +0900

    setup project

---
+++ MoyaSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoyaSample
+//
+//  Created by 藤井陽介 on 2019/07/17.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
